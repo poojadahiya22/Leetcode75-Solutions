a953bcd235e02cab666459f1770e349147c2446a
Pooja Dahiya <poojadahiya5647@gmail.com>
2026-03-06 10:13:00 +0530

    Implement function to reverse words in a string

---
+++ Array-String/06_reverse_words_ina_string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result = "";
+        int i = s.length() - 1;
+        while(i >= 0) {
+            while(i >= 0 && s[i] == ' ')
+                i--;
+                if(i < 0) break;
+                int j = i;
+                while(i >= 0 && s[i] != ' ')
+                i--;
+                if(!result.empty())
+                result += " ";
+                result += s.substr(i + 1, j - i);
+        }
+
+        return result;
+    }
+};
